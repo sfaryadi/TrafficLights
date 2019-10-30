d57f132454c88baf9844cfc04665b19c97df1f6f
RH209s <RH209s@RH209-12.uncanet.unca.edu>
2019-10-30 17:49:48 -0400

    more work

--- TrafficLights/src/trafficlights/CarGeneration.java
+++ TrafficLights/src/trafficlights/CarGeneration.java
@@ -3,19 +3,15 @@ package trafficlights;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class CarGeneration  {
 
-	
-	
-	static LinkedList<Car> northQueue = new LinkedList<>();		
-	static LinkedList<Car> southQueue = new LinkedList<>();		
-	static LinkedList<Car> eastQueue = new LinkedList<>();		
-	static LinkedList<Car> westQueue = new LinkedList<>();	
+	static LinkedList<Car> carQueue = new LinkedList<>();		
 
 	
-	
+
 	public static void main(String[] args) throws Exception {
 		
 
@@ -25,7 +21,10 @@ public class CarGeneration  {
 		for (int i = 0; i < 10; i++) {
 			Car newCar = new Car(scan.nextInt(), scan.next().charAt(0),scan.nextInt(),scan.nextInt());
 			System.out.printf("Arrival Time = %d Direction = %c Cross Time %d ID = %d\n", newCar.aTime, newCar.dir, newCar.cTime, newCar.id);
-			if(newCar.dir == 'N') {
+			Event arrival = new Event("A", newCar.aTime);
+			Simulator.eventQ.add(arrival);
+			carQueue.add(newCar);
+			/*if(newCar.dir == 'N') {
 				northQueue.add(newCar);
 			}
 			if(newCar.dir == 'S') {
@@ -37,12 +36,9 @@ public class CarGeneration  {
 			}if(newCar.dir == 'W') {
 				westQueue.add(newCar);
 			}
-			
+			*/
 		}
-		
-	//	System.out.println(northQueue.size());
-	//	System.out.print(northQueue.getFirst().id);
-		
+	
 	
 		
 
--- TrafficLights/src/trafficlights/Simulator.java
+++ TrafficLights/src/trafficlights/Simulator.java
@@ -6,16 +6,26 @@ import java.util.Scanner;
 
 public class Simulator {
 	
+	static PriorityQueue<Event> eventQ = new PriorityQueue<>();
 
 	public static void main(String[] args) {
 		int startTime;
 		int endTime;
 		int changeFrequency;
-		boolean nlGreen;
-		boolean elGreen;
+		boolean nlGreen = true;
+		boolean elGreen = false;
 		int currentTime;
 		int lightChange;
+		int crossTime;
 		
+		 LinkedList<Car> northQueue = new LinkedList<>();		
+		 LinkedList<Car> southQueue = new LinkedList<>();		
+		 LinkedList<Car> eastQueue = new LinkedList<>();		
+		 LinkedList<Car> westQueue = new LinkedList<>();	
+		 LinkedList<Car> crossingQueue = new LinkedList<>();	
+
+		 
+		// Takes input from the user for start time, end time, and light change frequency
 		Scanner input = new Scanner(System.in);	
 		System.out.println("Please enter the starting time of the simulation");
 		startTime = input.nextInt();	
@@ -23,43 +33,46 @@ public class Simulator {
 		endTime = input.nextInt();
 		System.out.println("How frequently does the light change?");
 		changeFrequency = input.nextInt();
-
-		PriorityQueue<Event> eventQ = new PriorityQueue<>();
-		Event current = new Event("NG", startTime);
-		lightChange = startTime + changeFrequency;
+		input.close();
+		
+		Event current = new Event("A", startTime);
+		currentTime = startTime;
+		lightChange = currentTime + changeFrequency;
 		eventQ.add(current);
+		
+		
+		
 		
 				
 		while(!eventQ.isEmpty()) {
-			Car currentCar;
+			Car crossingCar;
+			Car newArrival;
 			currentTime = current.getTime();
 			Event test = eventQ.remove();
 			switch (test.getEventType()) {
 				case "NG":
-				
+						
+					elGreen = false;
+					nlGreen = true;
+	
+					crossTime = northQueue.peek().cTime;
 					
-					
-					elGreen = true;
-					nlGreen = false;
-					int crossTime = CarGeneration.northQueue.peek().cTime;
-					
-					if(lightChange > currentTime + crossTime)
-					while (nlGreen == true) {
-						currentCar = CarGeneration.northQueue.remove();
-						Event crossing = new Event("C", currentTime + currentCar.cTime );
-						currentTime += currentCar.cTime;
+					if(lightChange > currentTime + crossTime) {
+						Event crossing = new Event("C", currentTime + crossTime);
 						eventQ.add(crossing);
 		
 					}
 					break;
 					
 				case "EG":
+					
 					elGreen = true;
 					nlGreen = false;
+					 crossTime = eastQueue.peek().cTime;
 					while (elGreen == true) {
-						currentCar = CarGeneration.eastQueue.remove();
-						Event crossing = new Event("C", currentTime + currentCar.cTime );
-						currentTime += currentCar.cTime;
+					//	crossingCar = eastQueue.remove();
+						Event crossing = new Event("C", currentTime + crossingCar.cTime );
+						currentTime += crossingCar.cTime;
 						eventQ.add(crossing);
 		
 					}
@@ -68,8 +81,52 @@ public class Simulator {
 					break; 
 					
 				case "C":
+					if (nlGreen == true && !northQueue.isEmpty()) {
+						crossingCar = northQueue.remove();
+						System.out.printf("Car %d has crossed the intersection" , crossingCar.id);
+						currentTime += crossingCar.cTime;
+					}
+					else if (elGreen == true && !eastQueue.isEmpty()) {
+						crossingCar = eastQueue.remove();
+						System.out.printf("Car %d has crossed the intersection" , crossingCar.id);
+
+						currentTime += crossingCar.cTime;
+					}
+					
+					
+					
 					break;
 				case "A":
+					elGreen = false;
+					nlGreen = true;
+					
+					newArrival = CarGeneration.carQueue.remove();
+					if(newArrival.dir == 'N') {
+						northQueue.add(newArrival);
+						System.out.printf("Car %d has arrive in the %C queue", newArrival.id,newArrival.dir);
+						crossTime = northQueue.peek().cTime;
+						
+						if(nlGreen == true && lightChange > currentTime + crossTime) {
+							Event crossing = new Event("C", currentTime + crossTime);
+							crossingQueue.add(newArrival);
+							eventQ.add(crossing);
+			
+						}
+					}
+					if(newArrival.dir == 'S') {
+						southQueue.add(newArrival);
+						System.out.printf("Car %d has arrive in the %C queue", newArrival.id,newArrival.dir);
+
+					}if(newArrival.dir == 'E') {
+						eastQueue.add(newArrival);
+						System.out.printf("Car %d has arrive in the %C queue", newArrival.id,newArrival.dir);
+
+					}if(newArrival.dir == 'W') {
+						westQueue.add(newArrival);
+						System.out.printf("Car %d has arrive in the %C queue", newArrival.id,newArrival.dir);
+
+					}
+				
 				
 					
 					break;
